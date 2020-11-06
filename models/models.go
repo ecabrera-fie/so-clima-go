6bc7193597fa2fe9746d4712fef43f84f2c5a63d
Ezequiel Cabrera <ezequiel.cabrera@appdirect.com>
2020-11-05 22:45:38 -0300

    Formateo archivos

--- models/models.go
+++ models/models.go
@@ -1,20 +1,21 @@
 package models
 
 type Response struct {
-	Status  string         `json:"status"`
-	Payload interface{}    `json:"payload"`
+	Status  string      `json:"status"`
+	Payload interface{} `json:"payload"`
 }
 
 type Weather struct {
-	MeanTemp          float64           `json:"temperaturaPromedio"`
-	StandardDeviation float64           `json:"desviacionEstandar"`
-	MinTemp           float64           `json:"temperaturaMinima"`
-	MaxTemp           float64           `json:"temperaturaMaxima"`
-	WeatherProviders  []WeatherProvider `json:"proveedores"`
+	MeanTemp            float64           `json:"temperaturaPromedio"`
+	StandardDeviation   float64           `json:"desviacionEstandar"`
+	MinTemp             float64           `json:"temperaturaMinima"`
+	MaxTemp             float64           `json:"temperaturaMaxima"`
+	SensorsDesestimated int               `json:"sensoresDescartados"`
+	WeatherProviders    []WeatherProvider `json:"proveedores"`
 }
 
 type WeatherProvider struct {
 	Name   string `json:"nombre"`
 	Status string `json:"estado"`
-	Error string `json:"error"`
+	Error  string `json:"error"`
 }
